981cf15abd42f80f7960522dde59b1543313ceae
Jorge Gonzalez <jorgeeldisg30@gmail.com>
2023-07-08 14:20:59 -0500

    Utilizando GUI

--- spectophotometerBarGraph.py
+++ spectophotometerBarGraph.py
@@ -1,68 +1,133 @@
 import serial
 import matplotlib.pyplot as plt
+import wx
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.figure import Figure
 
-# Set up the serial connection
-ser = serial.Serial('COM4', 9600)  # Replace 'COM4' with the appropriate port name and baud rate
+# Configure la conexión en serie
+ser = serial.Serial('COM4', 9600)
 
-# Create empty lists to store the data
+# Crear listas vacías para almacenar los datos
 x_data = []
 y_data = []
 
+# Configurar el estilo de la gráfica de Matplotlib
 plt.style.use('dark_background')
 
-# Set up the plot
-plt.ion()  # Enable interactive mode
-fig, ax = plt.subplots()
+# Crear la figura de Matplotlib
+fig = Figure()
+ax = fig.add_subplot(111)
+line, = ax.plot([], [], drawstyle='steps-pre', linewidth=2)  # Crear una gráfica vacía
+ax.set_xlim(400, 700)  # Establecer los límites del eje x
 
-# Create a file to save the data
-filename = 'serial_data_barra.txt'
+# Crear un archivo para guardar los datos
+filename = 'serial_data_lineal.txt'
 file = open(filename, 'w')
 
-# Continuously read and plot the serial data
-while True:
-    try:
-        # Read a line of data from the serial port
-        data = ser.readline().decode().strip()
+class MyFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super().__init__(parent, title=title, size=(800, 600))
+        
+        # Crear el lienzo de dibujo de Matplotlib
+        self.canvas = FigureCanvas(self, -1, fig)
+        
+        # Crear los botones
+        baseline_btn = wx.Button(self, label='Baseline')
+        single_btn = wx.Button(self, label='Single')
+        continuous_btn = wx.Button(self, label='Continuous')
+        
+        # Establecer un tamaño mínimo más grande para los botones
+        baseline_btn.SetMinSize((120, 40))
+        single_btn.SetMinSize((120, 40))
+        continuous_btn.SetMinSize((120, 40))
+        
+        # Crear el diseño de la ventana
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.canvas, 1, wx.EXPAND)
+        
+        # Crear un sizer horizontal para los botones
+        btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        btn_sizer.Add(baseline_btn, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        btn_sizer.AddSpacer(20)  # Agregar espacio entre los botones
+        btn_sizer.Add(single_btn, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        btn_sizer.AddSpacer(20)  # Agregar espacio entre los botones
+        btn_sizer.Add(continuous_btn, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        
+        sizer.Add(btn_sizer, 0, wx.ALIGN_CENTER)
+        self.SetSizer(sizer)
+        
+        # Vincular eventos a los botones
+        baseline_btn.Bind(wx.EVT_BUTTON, self.on_baseline_btn)
+        single_btn.Bind(wx.EVT_BUTTON, self.on_single_btn)
+        continuous_btn.Bind(wx.EVT_BUTTON, self.on_continuous_btn)
+        
+        # Configurar el temporizador para actualizar la gráfica en tiempo real
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.on_timer)
+        self.timer.Start(100)  # Actualizar cada 100 ms
+    
+    def on_baseline_btn(self, event):
+        # Lógica para el botón Baseline
+        print('Baseline button clicked')
+    
+    def on_single_btn(self, event):
+        # Lógica para el botón Single
+        print('Single button clicked')
+    
+    def on_continuous_btn(self, event):
+        # Lógica para el botón Continuous
+        print('Continuous button clicked')
+    
+    def on_timer(self, event):
+        try:
+            # Leer una línea de datos del puerto serie
+            data = ser.readline().decode().strip()
 
-        # Split the data into x and y values
-        values = data.split(',')
-        if len(values) != 2:
-            continue
+            # Dividir los datos en valores x e y
+            values = data.split(',')
+            if len(values) != 2:
+                return
 
-        x, y = map(float, values)
+            x, y = map(float, values)
 
-        # Check if the cycle repeats
-        if x_data and x < x_data[-1]:
-            # Clear the plot and reset the data lists
-            ax.clear()
-            x_data = []
-            y_data = []
+            # Comprobar si el ciclo se repite
+            if x_data and x < x_data[-1]:
+                # Borrar los datos y restablecer las listas
+                line.set_data([], [])
+                x_data.clear()
+                y_data.clear()
 
-        # Append the data to the lists
-        x_data.append(x)
-        y_data.append(y)
+            # Agregar los datos a las listas
+            x_data.append(x)
+            y_data.append(y)
 
-        # Plot the bar graph
-        ax.plot(x_data, y_data, drawstyle='steps-pre', linewidth=2)
+            # Actualizar la gráfica
+            line.set_data(x_data, y_data)
+            ax.relim()
+            ax.autoscale_view()
 
-        # Set the x-axis limits
-        ax.set_xlim(400, 700)
+            # Set labels and title
+            ax.set_xlabel('Wavelength (nm)')
+            ax.set_ylabel('Absorbance (a.u.)')
+            ax.set_title('Absorbance vs. Wavelength')
 
-        # Set labels and title
-        ax.set_xlabel('Wavelength (nm)')
-        ax.set_ylabel('Absorbance (a.u.)')
-        ax.set_title('Absorbance vs. Wavelength')
+            # Redibujar la gráfica
+            self.canvas.draw()
 
-        # Save the data to the file
-        file.write(f'{x},{y}\n')
+            # Guardar los datos en el archivo de texto
+            file.write(f'{x},{y}\n')
 
-        # Redraw the plot
-        plt.draw()
-        plt.pause(0.1)
+        except serial.SerialException:
+            print('Error: No se pudo abrir el puerto serie.')
 
-    except KeyboardInterrupt:
-        break
+class MyApp(wx.App):
+    def OnInit(self):
+        frame = MyFrame(None, 'Spectophotometer Linear Graph')
+        frame.Show()
+        return True
 
-# Close the file and the serial connection
-file.close()
+app = MyApp(redirect=False)
+app.MainLoop()
+
 ser.close()
+file.close()
--- spectophotometerLinearGraph.py
+++ spectophotometerLinearGraph.py
@@ -1,6 +1,8 @@
 import serial
 import matplotlib.pyplot as plt
-from matplotlib import style
+import wx
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 # Configure la conexión en serie
 ser = serial.Serial('COM4', 9600)
@@ -9,63 +11,123 @@ ser = serial.Serial('COM4', 9600)
 x_data = []
 y_data = []
 
+# Configurar el estilo de la gráfica de Matplotlib
 plt.style.use('dark_background')
 
-# Configuramos la grafica
-plt.ion() # Habilitar el modo interactivo
-fig, ax = plt.subplots()
-line, = ax.plot([], [])  # Crear una grafica vacía
-
-# Establecer los límites del eje x
-ax.set_xlim(400, 700)
+# Crear la figura de Matplotlib
+fig = Figure()
+ax = fig.add_subplot(111)
+line, = ax.plot([], [])  # Crear una gráfica vacía
+ax.set_xlim(400, 700)  # Establecer los límites del eje x
 
 # Crear un archivo para guardar los datos
 filename = 'serial_data_lineal.txt'
 file = open(filename, 'w')
 
-# Leer y trazar continuamente los datos en serie
-while True:
-    try:
+class MyFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super().__init__(parent, title=title, size=(800, 600))
+        
+        # Crear el lienzo de dibujo de Matplotlib
+        self.canvas = FigureCanvas(self, -1, fig)
+        
+        # Crear los botones
+        baseline_btn = wx.Button(self, label='Baseline')
+        single_btn = wx.Button(self, label='Single')
+        continuous_btn = wx.Button(self, label='Continuous')
+        
+        # Establecer un tamaño mínimo más grande para los botones
+        baseline_btn.SetMinSize((120, 40))
+        single_btn.SetMinSize((120, 40))
+        continuous_btn.SetMinSize((120, 40))
+        
+        # Crear el diseño de la ventana
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.canvas, 1, wx.EXPAND)
+        
+        # Crear un sizer horizontal para los botones
+        btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        btn_sizer.Add(baseline_btn, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        btn_sizer.AddSpacer(20)  # Agregar espacio entre los botones
+        btn_sizer.Add(single_btn, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        btn_sizer.AddSpacer(20)  # Agregar espacio entre los botones
+        btn_sizer.Add(continuous_btn, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        
+        sizer.Add(btn_sizer, 0, wx.ALIGN_CENTER)
+        self.SetSizer(sizer)
+        
+        # Vincular eventos a los botones
+        baseline_btn.Bind(wx.EVT_BUTTON, self.on_baseline_btn)
+        single_btn.Bind(wx.EVT_BUTTON, self.on_single_btn)
+        continuous_btn.Bind(wx.EVT_BUTTON, self.on_continuous_btn)
+        
+        # Configurar el temporizador para actualizar la gráfica en tiempo real
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.on_timer)
+        self.timer.Start(100)  # Actualizar cada 100 ms
+    
+    def on_baseline_btn(self, event):
+        # Lógica para el botón Baseline
+        print('Baseline button clicked')
+    
+    def on_single_btn(self, event):
+        # Lógica para el botón Single
+        print('Single button clicked')
+    
+    def on_continuous_btn(self, event):
+        # Lógica para el botón Continuous
+        print('Continuous button clicked')
+    
+    def on_timer(self, event):
+        try:
+            # Leer una línea de datos del puerto serie
+            data = ser.readline().decode().strip()
 
-        # Leer una línea de datos del puerto serie
-        data = ser.readline().decode().strip()
+            # Dividir los datos en valores x e y
+            values = data.split(',')
+            if len(values) != 2:
+                return
 
-        # Dividir los datos en valores x e y
-        values = data.split(',')
-        if len(values) != 2:
-            continue
+            x, y = map(float, values)
 
-        x, y = map(float, values)
+            # Comprobar si el ciclo se repite
+            if x_data and x < x_data[-1]:
+                # Borrar los datos y restablecer las listas
+                line.set_data([], [])
+                x_data.clear()
+                y_data.clear()
 
-        # Comprobar si el ciclo se repite
-        if x_data and x < x_data[-1]:
-            # Borrar la trama y restablecer las listas de datos
-            line.set_data([], [])
-            x_data = []
-            y_data = []
+            # Agregar los datos a las listas
+            x_data.append(x)
+            y_data.append(y)
 
-        # Agregar los datos a las listas
-        x_data.append(x)
-        y_data.append(y)
+            # Actualizar la gráfica
+            line.set_data(x_data, y_data)
+            ax.relim()
+            ax.autoscale_view()
 
-        # Actualizamos la grafica
-        line.set_data(x_data, y_data)
-        ax.relim()
-        ax.autoscale_view()
+            # Set labels and title
+            ax.set_xlabel('Wavelength (nm)')
+            ax.set_ylabel('Absorbance (a.u.)')
+            ax.set_title('Absorbance vs. Wavelength')
 
-        # Establecer etiquetas y título
-        ax.set_xlabel('Wavelength (nm)')
-        ax.set_ylabel('Absorbance (a.u.)')
-        ax.set_title('Absorbance vs. Wavelength')
+            # Redibujar la gráfica
+            self.canvas.draw()
 
-         # Guardar los datos en el archivo de texto
-        file.write(f'{x},{y}\n')
+            # Guardar los datos en el archivo de texto
+            file.write(f'{x},{y}\n')
 
-        # Redibujar la trama
-        plt.draw()
-        plt.pause(0.1)
+        except serial.SerialException:
+            print('Error: No se pudo abrir el puerto serie.')
 
-    except KeyboardInterrupt:
-        break
+class MyApp(wx.App):
+    def OnInit(self):
+        frame = MyFrame(None, 'Spectophotometer Linear Graph')
+        frame.Show()
+        return True
+
+app = MyApp(redirect=False)
+app.MainLoop()
 
 ser.close()
+file.close()
